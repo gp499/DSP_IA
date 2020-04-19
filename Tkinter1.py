f2306e73663c0053428b288a66a29d519b19544f
gp499 <guruananthabhat@gmail.com>
2020-04-19 16:07:03 +0530

    no changes

--- Tkinter1.py
+++ Tkinter1.py
@@ -20,6 +20,7 @@ def but_clear(number):
     number1=""
     input_text.set("")
 
+def but_encode
 input_text = StringVar()
 input_frame = Frame(window, width=312, height=50)
 input_frame.pack(side=TOP)
@@ -31,25 +32,25 @@ input_field.pack(ipady=10)
 btns_frame = Frame(window, width=312, height=272.5, bg='grey')
 btns_frame.pack()
 
-bt7 = Button(btns_frame, text="7", width=10, height=3, bg="orange", fg="red", command=lambda: but_click("7")).grid(column=1, row=1)
-bt8 = Button(btns_frame, text="8", width=10, height=3, bg="orange", fg="red", command=lambda: but_click("8")).grid(column=2, row=1)
-bt9 = Button(btns_frame, text="9", width=10, height=3, bg="orange", fg="red", command=lambda: but_click("9")).grid(column=3, row=1)
-btD = Button(btns_frame, text="D", width=10, height=3, bg="orange", fg="red", command=lambda: but_click("D")).grid(column=4, row=1)
+bt1 = Button(btns_frame, text="1", width=10, height=3, bg="orange", fg="red", command=lambda: but_click("1")).grid(column=1, row=3)
+bt2 = Button(btns_frame, text="2", width=10, height=3, bg="orange", fg="red", command=lambda: but_click("2")).grid(column=2, row=3)
+bt3 = Button(btns_frame, text="3", width=10, height=3, bg="orange", fg="red", command=lambda: but_click("3")).grid(column=3, row=3)
+btA =     Button(btns_frame, text="A", width=10, height=3, bg="orange", fg="red", command=lambda: but_click("A")).grid(column=4, row=4)
 
 bt4 = Button(btns_frame, text="4", width=10, height=3, bg="orange", fg="red", command=lambda: but_click("4")).grid(column=1, row=2)
 bt5 = Button(btns_frame, text="5", width=10, height=3, bg="orange", fg="red", command=lambda: but_click("5")).grid(column=2, row=2)
 bt6 = Button(btns_frame, text="6", width=10, height=3, bg="orange", fg="red", command=lambda: but_click("6")).grid(column=3, row=2)
-btC = Button(btns_frame, text="C", width=10, height=3, bg="orange", fg="red", command=lambda: but_click("C")).grid(column=4, row=2)
-
-bt1 = Button(btns_frame, text="1", width=10, height=3, bg="orange", fg="red", command=lambda: but_click("1")).grid(column=1, row=3)
-bt2 = Button(btns_frame, text="2", width=10, height=3, bg="orange", fg="red", command=lambda: but_click("2")).grid(column=2, row=3)
-bt3 = Button(btns_frame, text="3", width=10, height=3, bg="orange", fg="red", command=lambda: but_click("3")).grid(column=3, row=3)
 btB = Button(btns_frame, text="B", width=10, height=3, bg="orange", fg="red", command=lambda: but_click("B")).grid(column=4, row=3)
 
+bt7 = Button(btns_frame, text="7", width=10, height=3, bg="orange", fg="red", command=lambda: but_click("7")).grid(column=1, row=1)
+bt8 = Button(btns_frame, text="8", width=10, height=3, bg="orange", fg="red", command=lambda: but_click("8")).grid(column=2, row=1)
+bt9 = Button(btns_frame, text="9", width=10, height=3, bg="orange", fg="red", command=lambda: but_click("9")).grid(column=3, row=1)
+btC = Button(btns_frame, text="C", width=10, height=3, bg="orange", fg="red", command=lambda: but_click("C")).grid(column=4, row=2)
+
 bt_star = Button(btns_frame, text="*", width=10, height=3, bg="orange", fg="red", command=lambda: but_click("*")).grid(column=1, row=4)
-bt0 =     Button(btns_frame, text="0", width=10, height=3, bg="orange", fg="red", command=lambda: but_click("0")).grid(column=2, row=4)
+bt0 = Button(btns_frame, text="0", width=10, height=3, bg="orange", fg="red", command=lambda: but_click("0")).grid(column=2, row=4)
 bt_hash = Button(btns_frame, text="#", width=10, height=3, bg="orange", fg="red", command=lambda: but_click("#")).grid(column=3, row=4)
-btA =     Button(btns_frame, text="A", width=10, height=3, bg="orange", fg="red", command=lambda: but_click("A")).grid(column=4, row=4)
+btD = Button(btns_frame, text="D", width=10, height=3, bg="orange", fg="red", command=lambda: but_click("D")).grid(column=4, row=1)
 
 bt_play = Button(btns_frame, text="PLAY", width=10, height=3, bg="orange", fg="red", command=lambda: but_click("PLAY")).grid(column=1, row=5)
 bt_encode = Button(btns_frame, text="ENCODE", width=10, height=3, bg="orange", fg="red", command=lambda: but_click("ENCODE")).grid(column=2, row=5)
