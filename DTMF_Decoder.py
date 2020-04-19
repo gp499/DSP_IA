2a59245ad4b7af28c7d79642471835823985bdb4
gp499 <guruananthabhat@gmail.com>
2020-04-19 15:54:57 +0530

    wav output compatibility OOP applied on encoder and decoder

--- DTMF_Decoder.py
+++ DTMF_Decoder.py
@@ -1,4 +1,4 @@
-import wave
+from scipy.io import wavfile
 import math
 
 
@@ -22,57 +22,57 @@ class PygoertzelDtmf:
             self.coeff[k] = 2.0*math.cos(2.0 * math.pi * normalizedfreq)
 
     def __get_number(self, freqs):
-        hi = [1209.0,1336.0,1477.0,1633.0]
-        lo = [697.0,770.0,852.0,941.0]
+        hi = [1209.0, 1336.0, 1477.0, 1633.0]
+        lo = [697.0, 770.0, 852.0, 941.0]
         # get hi freq
         hifreq = 0.0
         hifreq_v = 0.0
         for f in hi:
-            if freqs[f]>hifreq_v:
+            if freqs[f] > hifreq_v:
                 hifreq_v = freqs[f]
                 hifreq = f
         # get lo freq
         lofreq = 0.0
         lofreq_v = 0.0
         for f in lo:
-            if freqs[f]>lofreq_v:
+            if freqs[f] > lofreq_v:
                 lofreq_v = freqs[f]
                 lofreq = f
-        if lofreq==697.0:
-            if hifreq==1209.0:
+        if lofreq == 697.0:
+            if hifreq == 1209.0:
                 return "1"
-            elif hifreq==1336.0:
+            elif hifreq == 1336.0:
                 return "2"
-            elif hifreq==1477.0:
+            elif hifreq == 1477.0:
                 return "3"
-            elif hifreq==1633.0:
+            elif hifreq == 1633.0:
                 return "A"
-        elif lofreq==770.0:
-            if hifreq==1209.0:
+        elif lofreq == 770.0:
+            if hifreq == 1209.0:
                 return "4"
-            elif hifreq==1336.0:
+            elif hifreq == 1336.0:
                 return "5"
-            elif hifreq==1477.0:
+            elif hifreq == 1477.0:
                 return "6"
-            elif hifreq==1633.0:
+            elif hifreq == 1633.0:
                 return "B"
-        elif lofreq==852.0:
-            if hifreq==1209.0:
+        elif lofreq == 852.0:
+            if hifreq == 1209.0:
                 return "7"
             elif hifreq==1336.0:
                 return "8"
-            elif hifreq==1477.0:
+            elif hifreq == 1477.0:
                 return "9"
-            elif hifreq==1633.0:
+            elif hifreq == 1633.0:
                 return "C"
-        elif lofreq==941.0:
-            if hifreq==1209.0:
+        elif lofreq == 941.0:
+            if hifreq == 1209.0:
                 return "*"
-            elif hifreq==1336.0:
+            elif hifreq == 1336.0:
                 return "0"
-            elif hifreq==1477.0:
+            elif hifreq == 1477.0:
                 return "#"
-            elif hifreq==1633.0:
+            elif hifreq == 1633.0:
                 return "D"
 
     def run(self, sample):
@@ -81,42 +81,75 @@ class PygoertzelDtmf:
             s = sample + (self.coeff[freq] * self.s_prev[freq]) - self.s_prev2[freq]
             self.s_prev2[freq] = self.s_prev[freq]
             self.s_prev[freq] = s
-            self.N[freq]+=1
-            power = (self.s_prev2[freq]*self.s_prev2[freq]) + (self.s_prev[freq]*self.s_prev[freq]) - (self.coeff[freq]*self.s_prev[freq]*self.s_prev2[freq])
-            self.totalpower[freq]+=sample*sample
-            if (self.totalpower[freq] == 0):
+            self.N[freq] += 1
+            power = (self.s_prev2[freq]*self.s_prev2[freq]) + (self.s_prev[freq]*self.s_prev[freq]) \
+                    - (self.coeff[freq]*self.s_prev[freq]*self.s_prev2[freq])
+            self.totalpower[freq] += sample*sample
+            if self.totalpower[freq] == 0:
                 self.totalpower[freq] = 1
             freqs[freq] = power / self.totalpower[freq] / self.N[freq]
         return self.__get_number(freqs)
 
 
-if __name__ == '__main__':
-    # load wav file
-    wav = wave.open('C:/Users/91845/PycharmProjects/DSP_IA/op.wav', 'r')#C:\Users\91845\PycharmProjects\DSP_IA\op.wav
-    (nchannels, sampwidth, framerate, nframes, comptype, compname) = wav.getparams()
-    frames = wav.readframes(nframes * nchannels)
-    # convert wave file to array of integers
-    #frames = struct.unpack_from("%dH"%nframes*nchannels, frames)
-    # if stereo get left/right
-    if nchannels == 2:
-        left = [frames[i] for i in range(0,len(frames),2)]
-        right = [frames[i] for i in range(1,len(frames),2)]
-    else:
-        left = frames
-        right = left
-    binsize = 400
-    # Split the bin in 4 to average out errors due to noise
-    binsize_split = 4
-    prevvalue = ""
-    prevcounter = 0
-    for i in range(0,len(left)-binsize,int(binsize/binsize_split)):
-        goertzel = PygoertzelDtmf(framerate)
-        for j in left[i:i+binsize]:
-            value = goertzel.run(j)
-        if value==prevvalue:
-            prevcounter+=1
-            if prevcounter==10:
-                print(value)
+class DtmfDecoder:
+    def __init__(self):
+        self.framerate, frames = wavfile.read('C:/Users/91845/PycharmProjects/DSP_IA/op.wav', 'r')
+        nchannels = 1
+        # if stereo get left/right
+        if nchannels == 2:
+            self.left = [frames[i] for i in range(0, len(frames), 2)]
+            self.right = [frames[i] for i in range(1, len(frames), 2)]
         else:
-            prevcounter=0
-            prevvalue=value
+            self.left = frames
+            self.right = self.left
+        self.binsize = 400
+        # Split the bin in 4 to average out errors due to noise
+        self.binsize_split = 4
+        self.prevvalue = ""
+        self.prevcounter = 0
+
+    def decode(self,):
+        for i in range(0, len(self.left) - self.binsize, int(self.binsize / self.binsize_split)):
+            goertzel = PygoertzelDtmf(self.framerate)
+            for j in self.left[i:i + self.binsize]:
+                value = goertzel.run(j)
+            if value == self.prevvalue:
+                self.prevcounter += 1
+                if self.prevcounter == 10:
+                    print(value)
+            else:
+                self.prevcounter = 0
+                self.prevvalue = value
+
+
+if __name__ == '__main__':
+    dtmf = DtmfDecoder()
+    dtmf.decode()
+
+    # load wav file
+    # framerate, frames = wavfile.read('C:/Users/91845/PycharmProjects/DSP_IA/op.wav', 'r')
+    # nchannels = 1
+    # sampwidth = 1
+    # # if stereo get left/right
+    # if nchannels == 2:
+    #     left = [frames[i] for i in range(0,len(frames),2)]
+    #     right = [frames[i] for i in range(1,len(frames),2)]
+    # else:
+    #     left = frames
+    #     right = left
+    # binsize = 400
+    # # Split the bin in 4 to average out errors due to noise
+    # binsize_split = 4
+    # prevvalue = ""
+    # prevcounter = 0
+    # for i in range(0,len(left)-binsize, int(binsize/binsize_split)):
+    #     goertzel = PygoertzelDtmf(framerate)
+    #     for j in left[i:i+binsize]:
+    #         value = goertzel.run(j)
+    #     if value == prevvalue:
+    #         prevcounter += 1
+    #         if prevcounter == 10:
+    #             print(value)
+    #     else:
+    #         prevcounter = 0
+    #         prevvalue = value
--- DTMF_Encoder.py
+++ DTMF_Encoder.py
@@ -3,51 +3,100 @@ import numpy as np
 from scipy.io import wavfile
 
 
-def main():
-    # Playback parameter
-    silence_length = 0.1
-    tone_length = 0.5
-    sampling_freq = 8000
-    volume = 0.5
-    freq = [697.0, 770.0, 852.0, 941.0, 1209.0, 1336.0, 1477.0, 1633.0]
-    tones = {
-    '1': (freq[0],freq[4]),
-    '2': (freq[0],freq[5]),
-    '3': (freq[0],freq[6]),
-    'A': (freq[0],freq[7]),
-    '4': (freq[1],freq[4]),
-    '5': (freq[1],freq[5]),
-    '6': (freq[1],freq[6]),
-    'B': (freq[1],freq[7]),
-    '7': (freq[2],freq[4]),
-    '8': (freq[2],freq[5]),
-    '9': (freq[2],freq[6]),
-    'C': (freq[2],freq[7]),
-    'F': (freq[3],freq[4]),
-    '0': (freq[3],freq[5]),
-    'M': (freq[3],freq[6]),
-    'D': (freq[3],freq[7])
-    }
-    t = np.linspace(0,tone_length,int(sampling_freq*tone_length)+1)
-    s = np.zeros(int(sampling_freq*silence_length),dtype='float32')
-    play = pyaudio.PyAudio()
-    sound = play.open(rate=sampling_freq, channels=1, format=pyaudio.paFloat32, output=True)
-    keys = input("Insert string:").upper()
-    keys = [k for k in keys if k in tones]
-    waveop = np.zeros(int(sampling_freq*silence_length),dtype='float32')
-    for k,key in enumerate(keys):
-        tone = tones[key]
-        wave = volume*np.sin(2.0*np.pi*t*tone[0]) + np.sin(2.0*np.pi*t*tone[1])
-        wave = wave.astype('float32')
-        waveop = np.append(waveop,wave)
-        sound.write(wave.tostring())
-        if k+1 != len(keys):
-            sound.write(s.tostring())
-    sound.close()
-    play.terminate()
-    wavfile.write('op.wav', sampling_freq, waveop.astype('float32'))
-    return
+class DtmfEncoder:
+    def __init__(self):
+        self.silence_length = 0.1
+        self.tone_length = 0.5
+        self.sampling_freq = 8000
+        self.volume = 0.5
+        freq = [697.0, 770.0, 852.0, 941.0, 1209.0, 1336.0, 1477.0, 1633.0]
+        self.tones = {
+            '1': (freq[0], freq[4]),
+            '2': (freq[0], freq[5]),
+            '3': (freq[0], freq[6]),
+            'A': (freq[0], freq[7]),
+            '4': (freq[1], freq[4]),
+            '5': (freq[1], freq[5]),
+            '6': (freq[1], freq[6]),
+            'B': (freq[1], freq[7]),
+            '7': (freq[2], freq[4]),
+            '8': (freq[2], freq[5]),
+            '9': (freq[2], freq[6]),
+            'C': (freq[2], freq[7]),
+            'F': (freq[3], freq[4]),
+            '0': (freq[3], freq[5]),
+            'M': (freq[3], freq[6]),
+            'D': (freq[3], freq[7])
+        }
+        self.t = np.linspace(0, self.tone_length, int(self.sampling_freq * self.tone_length) + 1)
+        self.s = np.zeros(int(self.sampling_freq * self.silence_length), dtype='float32')
+        self.play = pyaudio.PyAudio()
+        self.sound = self.play.open(rate=self.sampling_freq, channels=1, format=pyaudio.paFloat32, output=True)
+
+    def encoder(self,keys):
+        keys = [k for k in keys if k in self.tones]
+        waveop = np.zeros(int(self.sampling_freq * self.silence_length), dtype='float32')
+        for k, key in enumerate(keys):
+            tone = self.tones[key]
+            wave = self.volume * np.sin(2.0 * np.pi * self.t * tone[0]) + np.sin(2.0 * np.pi * self.t * tone[1])
+            wave = wave.astype('float32')
+            waveop = np.append(waveop, wave)
+            self.sound.write(wave.tostring())
+            if k + 1 != len(keys):
+                self.sound.write(self.s.tostring())
+        self.sound.close()
+        self.play.terminate()
+        wavfile.write('op.wav', self.sampling_freq, waveop.astype('float32'))
+        return
+
+#
+# def main():
+#     # Playback parameter
+#     silence_length = 0.1
+#     tone_length = 0.5
+#     sampling_freq = 8000
+#     volume = 0.5
+#     freq = [697.0, 770.0, 852.0, 941.0, 1209.0, 1336.0, 1477.0, 1633.0]
+#     tones = {
+#     '1': (freq[0],freq[4]),
+#     '2': (freq[0],freq[5]),
+#     '3': (freq[0],freq[6]),
+#     'A': (freq[0],freq[7]),
+#     '4': (freq[1],freq[4]),
+#     '5': (freq[1],freq[5]),
+#     '6': (freq[1],freq[6]),
+#     'B': (freq[1],freq[7]),
+#     '7': (freq[2],freq[4]),
+#     '8': (freq[2],freq[5]),
+#     '9': (freq[2],freq[6]),
+#     'C': (freq[2],freq[7]),
+#     'F': (freq[3],freq[4]),
+#     '0': (freq[3],freq[5]),
+#     'M': (freq[3],freq[6]),
+#     'D': (freq[3],freq[7])
+#     }
+#     t = np.linspace(0,tone_length,int(sampling_freq*tone_length)+1)
+#     s = np.zeros(int(sampling_freq*silence_length),dtype='float32')
+#     play = pyaudio.PyAudio()
+#     sound = play.open(rate=sampling_freq, channels=1, format=pyaudio.paFloat32, output=True)
+#     keys = input("Insert string:").upper()
+#     keys = [k for k in keys if k in tones]
+#     waveop = np.zeros(int(sampling_freq*silence_length),dtype='float32')
+#     for k,key in enumerate(keys):
+#         tone = tones[key]
+#         wave = volume*np.sin(2.0*np.pi*t*tone[0]) + np.sin(2.0*np.pi*t*tone[1])
+#         wave = wave.astype('float32')
+#         waveop = np.append(waveop,wave)
+#         sound.write(wave.tostring())
+#         if k+1 != len(keys):
+#             sound.write(s.tostring())
+#     sound.close()
+#     play.terminate()
+#     wavfile.write('op.wav', sampling_freq, waveop.astype('float32'))
+#     return
 
 
 if __name__ == '__main__':
-    main()
+    dtmf = DtmfEncoder()
+    keys = input("Insert string:").upper()
+    dtmf.encoder(keys)
